1b18803d0c382cd2a9ef60987105ff7e0a47384a
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:09:57 +0100

    Add Ejercicio 5

--- src/Jugador.java
+++ src/Jugador.java
@@ -2,6 +2,17 @@ public class Jugador extends Entidad {
     int experiencia;
     int nivel;
 
+    @Override
+    public int turno(int accion) {
+        if (accion == 0) {
+            return this.getAtaque() * this.nivel;
+        } else if (accion == 1) {
+            return this.getDefensa() * this.nivel;
+        } else {
+            return this.getAtaque();
+        }
+    }
+
     public void setExperiencia(int experiencia) {
         this.experiencia = experiencia;
     }
