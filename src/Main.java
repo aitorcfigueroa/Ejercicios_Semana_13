edb8fc2d7ba696ad57fd9fa61e71e5d45e24f642
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:16:25 +0100

    Add Ejercicio 7

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Jugador jugador = new Jugador();
+        Enemigo enemigo = new Enemigo();
+        Entidad [] entidad = new Entidad[2];
+
+        entidad[0] = jugador;
+        entidad[1] = enemigo;
+    }
+}
