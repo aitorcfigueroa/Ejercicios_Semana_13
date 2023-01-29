187accf8d09fbe48a24ed32ee6112f57e81892d2
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:02:04 +0100

    Add Ejercicio 3

---
+++ src/Enemigo.java
@@ -0,0 +1,11 @@
+public class Enemigo extends Entidad {
+    int dificultad;
+
+    public void setDificultad(int dificultad) {
+        this.dificultad = dificultad;
+    }
+
+    public int getDificultad() {
+        return dificultad;
+    }
+}
