8dde2167c655e25bae60fbc02fce04e819fa3657
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:03:36 +0100

    Add Ejercicio 1

--- src/Entidad.java
+++ src/Entidad.java
@@ -3,4 +3,36 @@ public class Entidad {
     int ataque;
     int defensa;
     String nombre;
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
