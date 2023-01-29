da22ae3341b4162fd9d5dba4a76af7348f9a1362
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:34:18 +0100

    Add Ejercicio 8

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,17 @@ public class Main {
 
         entidad[0] = jugador;
         entidad[1] = enemigo;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(entidad[0].turno(i));
+            System.out.println(entidad[1].turno(i));
+        }
+
+        /*
+        * En este caso, el uso de la herencia y el polimorfismo nos permitiría
+        * la creación de diferentes tipos de jugadores o enemigos, gracias a la herencia,
+        * y dotarlos de características propias a cada uno, gracias al polimorfismo,
+        * sin la necesidad de reescribir código.
+        */
     }
 }
