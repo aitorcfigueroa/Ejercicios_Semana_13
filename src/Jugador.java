8715f0ec50dcf8b17cc5563c003d1311f548370b
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:02:38 +0100

    Add Ejercicio 2

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,20 @@
 public class Jugador extends Entidad {
     int experiencia;
     int nivel;
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
 }
