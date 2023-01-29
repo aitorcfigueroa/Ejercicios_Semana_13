7248cbcea75e4b82e3c43f15d42dd515f8bca64b
Aitor C. Figueroa <aitorcf@gmail.com>
2023-01-29 10:12:30 +0100

    Add Ejercicio 6

--- src/Enemigo.java
+++ src/Enemigo.java
@@ -1,6 +1,17 @@
 public class Enemigo extends Entidad {
     int dificultad;
 
+    @Override
+    public int turno(int accion) {
+        if (accion == 0) {
+            return this.getAtaque() * this.dificultad;
+        } else if (accion == 1) {
+            return this.getDefensa() * this.dificultad;
+        } else {
+            return this.getDefensa();
+        }
+    }
+
     public void setDificultad(int dificultad) {
         this.dificultad = dificultad;
     }
